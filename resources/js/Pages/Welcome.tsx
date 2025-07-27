fbd3df657937ae5954680f6b7a7e298733f7f3a8
Putra <menggarongx@gmail.com>
2025-07-27 19:07:17 +0700

    push putra tambah kalender dan card di welkam

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,8 +1,20 @@
+import React from "react";
 import { Button } from "@/Components/ui/button";
 import { Switch } from "@/Components/ui/switch";
+import { Calendar } from "@/Components/ui/calendar";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/Components/ui/card"
+
 import Navbar from "@/Components/Navbar";
 
 export default function Home() {
+    const [date, setDate] = React.useState<Date | undefined>(new Date())
     return (
         <div className="w-full h-screen pt-10 bg-[#202020]">
             <Navbar></Navbar>
@@ -14,6 +26,22 @@ export default function Home() {
             </div>
             <Switch></Switch>
             <Button>Click Me</Button>
+            <Calendar  mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="rounded-lg border">
+                
+            </Calendar>
+            <Card>
+                <CardHeader>
+                <CardTitle>Putra</CardTitle>
+                <CardDescription>Putra Anwar Fadilah Ganteng</CardDescription>
+                </CardHeader>
+                <CardContent className="text-xl text-blue-400"><p>Putra</p></CardContent>
+                <CardFooter><p>Putra</p></CardFooter>
+                <Button>Klik aku</Button>
+                <Switch></Switch>
+            </Card>
         </div>
     );
 }
